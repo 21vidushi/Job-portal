01fd09360af22e23bbced4fd676480e088947b49
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-02-29 14:17:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,9 @@ app.use(
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
+app.get("/",(req,res)=>{
+  res.send("Hello");
+});
 dbConnection();
 
 app.use(errorMiddleware);
