e8eaae3e543b913d96b68d47cfe9b5d9c0e8ab06
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-03-01 01:40:37 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://job-portal-tcck.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
